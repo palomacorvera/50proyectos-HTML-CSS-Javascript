144e014d2c105581ca5fab65f7b8abb6e27e2cb3
Paloma Corvera <77248450+palomacorvera@users.noreply.github.com>
2021-08-26 02:24:06 +0200

    Delete 04_hiddenSearchWidget directory

--- 04_hiddenSearchWidget/script.js
+++
@@ -1,8 +0,0 @@
-const buscar = document.querySelector('.buscar');
-const boton = document.querySelector('.boton');
-const input = document.querySelector('.input');
-
-boton.addEventListener('click', () => {
-    buscar.classList.toggle('activo');
-    input.focus();
-})
