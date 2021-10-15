4bd2b302b401ab0ea5f1ae24193c954928a6c984
Paloma Corvera <77248450+palomacorvera@users.noreply.github.com>
2021-10-15 02:08:09 +0200

    Add files via upload

---
+++ 26_toastNotification/script.js
@@ -0,0 +1,40 @@
+const button = document.getElementById('button');
+const toasts = document.getElementById('toasts');
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four'
+];
+
+const types = [
+    'info',
+    'success',
+    'error'
+]
+
+button.addEventListener('click', () => createNotification());
+
+function createNotification() {
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+
+    notif.classList.add(getRandomType());
+
+    notif.innerText = getRandomMessage();
+     
+    toasts.appendChild(notif);
+
+    setTimeout(() => {
+        notif.remove();
+    }, 2000);
+};
+
+function getRandomMessage() {
+    return messages[Math.floor(Math.random() * messages.length)];
+};
+
+function getRandomType() {
+    return types[Math.floor(Math.random() * types.length)];
+};
